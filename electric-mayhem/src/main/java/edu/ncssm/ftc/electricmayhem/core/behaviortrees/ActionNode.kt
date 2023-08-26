ce3a64e2463e8a754aa26f0e0aa5b97618cd9245
Carl Ryden <caryden@alum.mit.edu>
2023-08-26 13:09:57 -0400

    added constructor to ActionNode that takes/wraps a subsystemcommand

--- electric-mayhem/src/main/java/edu/ncssm/ftc/electricmayhem/core/behaviortrees/ActionNode.kt
+++ electric-mayhem/src/main/java/edu/ncssm/ftc/electricmayhem/core/behaviortrees/ActionNode.kt
@@ -1,11 +1,18 @@
 package edu.ncssm.ftc.electricmayhem.core.behaviortrees
 
+import edu.ncssm.ftc.electricmayhem.core.subsystems.SubsystemCommand
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import subsystems.Subsystem
 
 class ActionNode(private val toExecute : suspend () -> NodeStatus)
     : Node {
+    constructor(subsystemCommand: SubsystemCommand) : this( {
+        subsystemCommand.execute()
+        NodeStatus.Success
+    })
+
     private  val statusFlow = MutableStateFlow<NodeStatus>(NodeStatus.Idle)
     override val status = statusFlow.asStateFlow()
 
